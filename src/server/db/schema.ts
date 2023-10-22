538ad09b931bc5d8b4b9fc26876a3c3fabcf142f
Mainak Das <mainakdas104@gmail.com>
2023-10-22 14:31:02 +0530

    Feat: Add short url schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -8,6 +8,8 @@ import {
   mysqlTableCreator,
   timestamp,
   varchar,
+  int,
+  boolean,
 } from "drizzle-orm/mysql-core";
 
 /**
@@ -18,17 +20,23 @@ import {
  */
 export const mysqlTable = mysqlTableCreator((name) => `clipsnip_${name}`);
 
-export const posts = mysqlTable(
-  "post",
+export const urls = mysqlTable(
+  "url",
   {
     id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
-    name: varchar("name", { length: 256 }),
+    url: varchar("url", { length: 256 }),
+    shortUrl: varchar("short_url", { length: 256 }),
+    userAuthId: varchar("user_id", { length: 256 }),
+    totalVisitCount: int("total_visit_count"),
+    disabled: boolean("disabled").default(false),
     createdAt: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
     updatedAt: timestamp("updatedAt").onUpdateNow(),
   },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+  (table) => ({
+    urlIndex: index("url_idx").on(table.url),
+    shortUrlIndex: index("short_url_idx").on(table.shortUrl),
+    userAuthIdIndex: index("user_id_idx").on(table.userAuthId),
+  }),
 );
