02a533dcb07e6758e2a7b96e026ed3efbef56721
Mainak Das <mainakdas104@gmail.com>
2023-10-22 14:31:40 +0530

    Fix: Add providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,20 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "~/components/ThemeProvider";
+import { Layout } from "~/components/Layout";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
