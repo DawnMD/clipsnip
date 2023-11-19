a22b1b668f6cd1593270b0f9a055f366d853795d
Mainak Das <mainakdas104@gmail.com>
2023-11-19 23:46:43 +0530

    Update link visit count and add geodata and date tracking

--- src/pages/api/url/redirect/[slug].ts
+++ src/pages/api/url/redirect/[slug].ts
@@ -1,15 +1,15 @@
-import { eq } from "drizzle-orm";
+import { eq, sql } from "drizzle-orm";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { db } from "~/server/db";
 import { urlSchema } from "~/server/db/schema";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { slug } = req.query;
+  const { slug,country } = req.query;
 
   if (!slug || typeof slug !== "string") {
     return res.status(400).json({
       error:
-        "[X] Error: Missing slug? Remember that urls start like this: /u/yourLink",
+        "[X] Error: Missing slug? Remember that urls start like this: /snip/yourLink",
     });
   }
 
@@ -20,7 +20,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (!data) {
     return res.status(404).json({
       error:
-        "[X] Error: Link not found or removed. Go to slug.vercel.app and create a new link.",
+        "[X] Error: Link not found or removed. Go to clipsnip.vercel.app and create a new link.",
     });
   }
 
@@ -29,12 +29,35 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
   // Update link visit count if not disabled:
   if (!data.disabled) {
+    // Update total visit count
     await db
       .update(urlSchema)
       .set({
         totalVisitCount: data.totalVisitCount + 1,
       })
       .where(eq(urlSchema.shortUrl, slug));
+
+    // Update unique geodata visit count
+    await db.execute(
+      sql.raw(`
+    UPDATE
+      clipsnip_url
+    SET
+      visit_count_by_country = JSON_SET(visit_count_by_country, '$."${country}"', COALESCE(JSON_EXTRACT(visit_count_by_country, '$."${country}"'), 0) + 1)
+    WHERE
+      short_url = "${slug}";
+`));
+
+    // Update visit count by date
+    await db.execute(
+      sql.raw(`
+    UPDATE
+      clipsnip_url
+    SET
+      visit_count_by_date = JSON_SET(visit_count_by_date, '$."${new Date().setHours(0,0,0,0)}"', COALESCE(JSON_EXTRACT(visit_count_by_date, '$."${new Date().setHours(0,0,0,0)}"'), 0) + 1)
+    WHERE
+      short_url = "${slug}";
+`));
   }
 
   return res.json(data);
